692affff073c53af1794254fd804a2dfbc64aebd
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-03-01 16:57:58 +0700

    Customize Project

---
+++ src/contents/project.ts
@@ -0,0 +1,62 @@
+import { Project } from "@/types";
+
+
+export const projects: Project[] = [
+    {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    }, {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/chat-app.png',
+    },
+];
