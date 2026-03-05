bf451df3624e550fafca201a885f89cbcffec79f
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-03-05 20:36:54 +0700

    Create Navbar Component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useState } from "react"
+
+const NavBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false)
+  const pathname = usePathname()
+  const theme = "dark"
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+  const menuItems = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/projects", label: "Projects" },
+    { href: "/blogs", label: "Blogs" },
+    { href: "/contact", label: "Contact" },
+  ]
+  return (
+    <nav className="fixed w-full bg-white/80 dark:bg-dark/80 backdrop-blur-sm z-50">
+      <div className="container max-w-7xl mx-auto px-4">
+        {/* Desktop Menu */}
+        <div className="flex items-center justify-between h-16">
+          {/* Logo */}
+          <Link href="/" className="text-xl font-bold text-primary">
+            DevPortfolio&trade;
+          </Link>
+          {/* List of menu */}
+          <div className="hidden md:flex items-center space-x-8">
+            {menuItems.map((item) => {
+              const isActive = pathname === item.href
+              return (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={`hover:text-primary transition-colors font-medium ${
+                    isActive ? " text-primary" : ""
+                  } `}
+                >
+                  {item.label}
+                </Link>
+              )
+            })}
+            <button
+              className="p-2 rounded-lg hover:bg-gray-100 text-primary
+             dark:hover:bg-gray-800 transition-colors cursor-pointer"
+            >
+              {theme === "dark" ? (
+                <SunIcon className="w-5 h-5" />
+              ) : (
+                <MoonIcon className="w-5 h-5" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import NavBar from "./components/Navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white`}
       >
-        <nav>Navbar</nav>
-        {children}
+        <NavBar />
+        <main className="min-h-screen pt-24">{children}</main>
         <footer>Footer</footer>
       </body>
     </html>
